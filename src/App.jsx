431ad4b5ce2540f9f6b935254a9d43d6a29dbf0c
khemissat <khemissat.ra@gmail.com>
2025-07-14 12:43:15 +0200

    fixing deployment bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Hero from "./Section/Hero"
 import Navbar from "./Section/Navbar"
 import Service from "./Section/Service"
 import About from "./Section/About"
-import Footer from "./Section/Footer"
+import Footer from "./Section/footer"
+
 import React from 'react'
 
 function App() {
